a71c295ab10df8b776221ab1c8c18a0b40b18099
Gnlow <entrydark@gmail.com>
2020-06-24 11:15:32 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,6 +1,14 @@
-const hash = (x: number) => {
-    const editedX = BigInt(Math.floor(x))
-    return Number( ( (0x0000FFFFn & editedX) << 16n ) 
-        + ( (0xFFFF0000n & editedX) >> 16n ) )
-}
-console.log(hash(Math.random() * 0xFFFFFFFF))
+import iArr from "https://raw.githubusercontent.com/gnlow/infinite-array/master/mod.ts"
+
+import {shuffle, random, unshuffle} from "https://raw.githubusercontent.com/gnlow/bitwise-shuffle/master/mod.ts"
+
+const key = random(4)
+const hash = shuffle(key)
+const unhash = unshuffle(key)
+
+const regen = iArr(n => unhash(n*4 + 1))
+const [
+    a, b, c, d, e
+] = regen as Iterable<number>
+console.log(a, b, c)
+console.log(hash(a), hash(b), hash(c))
