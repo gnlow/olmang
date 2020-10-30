a2fce4b4d64718e3b768ed7f730a9f492bdb61b5
Gnlow <entrydark@gmail.com>
2020-10-31 00:51:21 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,25 +1,9 @@
 import iArr from "https://raw.githubusercontent.com/gnlow/infinite-array/master/mod.ts"
 
-import {shuffle, random, unshuffle} from "https://raw.githubusercontent.com/gnlow/bitwise-shuffle/master/mod.ts"
-
 import {from} from "https://deno.land/x/lazy/mod.ts"
 
-import {Shuffler, Seed} from "./Shuffler.ts"
+import Modular from "./Modular.ts"
 
-class BwShuf extends Shuffler {
-    key
-    constructor(volume: number) {
-        super()
-        this.key = random(volume ** (1 / 2))
-    }
-    shuffle(seed: Seed) {
-        return shuffle(this.key)(seed)
-    }
-    unshuffle(value: Seed) {
-        return unshuffle(this.key)(value)
-    }
-}
-
-const shuffler = new BwShuf(16)
+const shuffler = new Modular(16)
 const regen = iArr(n => shuffler.unshuffle(n*5 + 1))
 console.log(from(regen).take(16).toArray().map(n => shuffler.shuffle(n)))
